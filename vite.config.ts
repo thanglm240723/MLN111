164e1f8d76c775b7728e02eea1f521e9702189d7
ThangLM <thanglm240723@gmail.com>
2026-01-27 21:59:21 +0700

    fix base lần N

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/mln111/",
+  base: "./",
 
   server: {
     host: "::",
